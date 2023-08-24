cc6903a6d134616763de1bbba61ffcea4dc823da
Tanmay Singh <tanmaysinghcg@gmail.com>
2023-08-25 00:22:57 +0530

    First commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static String appId ="1:283363838947:web:fffeab39eb8931af18aed6";
+  static String apiKey ="AIzaSyCKWDdPpJv4vVH5njZSmRPiulWdbEpTEjs";
+  static String messagingSenderId ="283363838947";
+  static String projectId ="chatappflutter-ca7bc";
+
+}
