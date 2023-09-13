386ba117755cb13dfac831ab2328040e516726e5
Tanmay Singh <tanmaysinghcg@gmail.com>
2023-09-13 15:47:25 +0530

    Third commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat_app/pages/auth/login_page.dart';
 import 'package:chat_app/pages/profile_page.dart';
 import 'package:chat_app/pages/search_page.dart';
+import 'package:chat_app/service/database_service.dart';
+import 'package:chat_app/widgets/group_tile.dart';
 import 'package:chat_app/widgets/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../helper/helper_function.dart';
@@ -18,12 +21,23 @@ class _HomePageState extends State<HomePage> {
   String userName="";
   String email ="";
   AuthService authService = AuthService();
+  Stream? groups;
+  bool _isLoading = false;
+  String groupName="";
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     gettingUserData();
+  }
+
+  String getId(String res){
+    return res.substring(0,res.indexOf("_"));
+  }
+
+  String getName(String res){
+    return res.substring(res.indexOf("_")+1);
   }
 
   gettingUserData() async{
@@ -35,6 +49,14 @@ class _HomePageState extends State<HomePage> {
     await HelperFunctions.getUserNameFromSF().then((val){
       setState(() {
         userName = val!;
+      });
+    });
+
+    await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+    .getUserGroups()
+    .then((snapshot){
+      setState(() {
+        groups=snapshot;
       });
     });
   }
@@ -54,7 +76,7 @@ class _HomePageState extends State<HomePage> {
         title: const Text(
           "Groups",
           style: TextStyle(
-            color: Colors.white,fontWeight: FontWeight.bold,fontSize: 27
+            color: Colors.white,fontWeight: FontWeight.bold,fontSize: 27,
           ),
         ),
       ),
@@ -87,7 +109,10 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               onTap: () {
-                nextScreen(context, const ProfilePage());
+                nextScreenReplace(context, ProfilePage(
+                  userName: userName,
+                  email: email,
+                ));
               },
 
               contentPadding:
@@ -99,9 +124,11 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             ListTile(
+
               onTap: () async{
                 showDialog(context: context, builder: (context){
                   return AlertDialog(
+
                     title: const Text("Log out"),
                     content: const Text("Are you sure you want to logout?"),
                     actions: [
@@ -131,6 +158,166 @@ class _HomePageState extends State<HomePage> {
             ),
           ]
         )
+      ),
+      body: groupList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          popUpDialog(context);
+        },
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size:30,
+        ),
+
+    ),
+
+    );
+  }
+
+  popUpDialog(BuildContext context){
+    showDialog(
+        barrierDismissible: true,context: context, builder: (context)
+    {
+      return StatefulBuilder(
+        builder: ((context,setState) {
+        return AlertDialog(
+          title: const Text("Create a Group",
+            textAlign: TextAlign.left,
+            ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children:[
+              _isLoading==true?
+              Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  )
+              ): TextField(
+                onChanged: (val){
+                  setState(() {
+                    groupName=val;
+
+                  });
+                },
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor
+                    ),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                      ),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor
+                      ),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+              ),
+            ]
+          ),
+          actions: [
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).pop();
+            }, child: const Text("CANCEL"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+            ),
+
+            ElevatedButton(onPressed: () async{
+              if(groupName!=""){
+                setState(() {
+                  _isLoading=true;
+                });
+                DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+                    .createGroup(userName, FirebaseAuth.instance.currentUser!.uid, groupName)
+                .whenComplete(() {
+                  _isLoading=false;
+                });
+                Navigator.of(context).pop();
+                showSnackBar(context, Colors.green, "Group created successfully.");
+              }
+            }, child: const Text("CREATE"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+            ),
+          ],
+
+        );})
+      );
+    });
+
+
+  }
+
+  groupList(){
+    return StreamBuilder(
+      stream: groups,
+      builder: (context, AsyncSnapshot snapshot){
+        if(snapshot.hasData){
+          if(snapshot.data['groups']!=null){
+            if(snapshot.data['groups'].length!=0){
+              return ListView.builder(
+                itemCount: snapshot.data['groups'].length,
+                itemBuilder: (context, index){
+                  int reverseIndex= snapshot.data['groups'].length-index-1;
+                  return GroupTile(
+
+                      groupId: getId(snapshot.data['groups'][reverseIndex]),
+                      groupName: getName(snapshot.data['groups'][reverseIndex]),
+                      userName: snapshot.data['fullName']);
+                },
+              );
+
+            }else{
+              return noGroupWidget();
+            }
+
+          }else{
+            return noGroupWidget();
+          }
+
+        }else{
+          return Center(
+            child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  noGroupWidget(){
+    return Container(
+    padding: const EdgeInsets.symmetric(horizontal:25),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap:(){
+              popUpDialog(context);
+            },
+            child: Icon(Icons.add_circle,color: Colors.grey[700], size: 75),
+          ),
+
+          const SizedBox(height:20),
+          const Text("You've not joined any group! Click the icon to join any group.",
+          textAlign: TextAlign.center,),
+        ]
       ),
     );
   }
--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -26,4 +26,30 @@ class DatabaseService{
     QuerySnapshot snapshot= await userCollection.where("email", isEqualTo: email).get();
     return snapshot;
   }
+
+  getUserGroups() async{
+    return userCollection.doc(uid).snapshots();
+  }
+
+  Future createGroup(String userName, String id, String groupName) async{
+    DocumentReference groupDocumentReference= await groupCollection.add({
+      "groupName": groupName,
+      "groupIcon": "",
+      "admin": "${id}_$userName",
+      "members":[],
+      "groupId":"",
+      "recentMessage": "",
+      "recentMessageSender": "",
+    });
+
+    await groupDocumentReference.update({
+      "members": FieldValue.arrayUnion(["{uid}_$userName"]),
+      "groupId": groupDocumentReference.id,
+    });
+
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    return await userDocumentReference.update({
+      "groups": FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"])
+    });
+  }
 }
